e2664ea0307f69d5f89fe29f6fafc12d43ef3694
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-19 13:50:36 +0100

    delete button created and tested

--- client/src/views/list_item_view.js
+++ client/src/views/list_item_view.js
@@ -1,4 +1,5 @@
 const createAndAppend = require('../helpers/create_append.js');
+const PubSub = require('../helpers/pub_sub.js');
 
 const ListItemView = function (data, container) {
   this.data = data;
@@ -13,7 +14,20 @@ ListItemView.prototype.render = function () {
   } else {
     completed = "Not Completed"
   }
-  createAndAppend('span', 'box', completed, li)
+  createAndAppend('span', 'box', completed, li);
+  this.createDeleteButton(li)
 };
+
+ListItemView.prototype.createDeleteButton = function (parent) {
+  const button = createAndAppend('button', 'delete-button', 'delete', parent);
+  button.value = this.data._id
+
+  button.addEventListener('click', (e) => {
+    PubSub.publish('ListItemView:delete-clicked', e.target.value);
+
+  })
+
+};
+
 
 module.exports = ListItemView;

6c1c4984e791b0c4444e843ccf874969fc59c0f3
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-19 11:37:14 +0100

    Complete form_view, tested and working

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -5,7 +5,30 @@ const FormView = function (form) {
 };
 
 FormView.prototype.bindEvents = function () {
-  console.log('FormView working');
+  this.form.addEventListener('submit', (e) => {
+    this.handleSubmit(e);
+  });
+
 };
+
+FormView.prototype.handleSubmit = function (e) {
+  e.preventDefault();
+  const newListItem = this.createElement(e.target);
+  PubSub.publish('FormView:submitted', newListItem);
+  console.log(newListItem);
+  e.target.reset();
+};
+
+FormView.prototype.createElement = function (form) {
+  const newListItem = {
+    name: form.name.value,
+    completed: form.completed.value
+  };
+
+  return newListItem
+};
+
+
+
 
 module.exports = FormView;
